97b1e9366e67e14218e516737522afa27507d18e
Asil Mian <asilmian@gmail.com>
2020-03-08 15:50:05 +1100

    added sign in and sign out features

---
+++ app/src/main/java/com/example/simpleapp/Home.java
@@ -0,0 +1,42 @@
+package com.example.simpleapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    private final static String TAG = Home.class.toString();
+    private Button logoutB = findViewById(R.id.logOutButton);
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mAuth = FirebaseAuth.getInstance();
+        logoutB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        mAuth.signOut();
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            Log.e(TAG, "logout: ", e);
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -1,14 +1,91 @@
 package com.example.simpleapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    EditText email = findViewById(R.id.emailText);
+    EditText password = findViewById(R.id.passText);
+    Button loginB = findViewById(R.id.loginButton);
+    Button signUpB = findViewById(R.id.signUpButton);
+
+    private final static String TAG = MainActivity.class.toString();
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+            }
+        });
+
+        signUpB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp(v);
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            showLoginScreen();
+        }
+    }
+
+    private void login(View v) {
+        String emailS = email.getText().toString();
+        String passwordS = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(emailS, passwordS)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(MainActivity.class.toString(), "Sign in with Email: Success");
+                            showLoginScreen();
+                        } else {
+                            Log.w(TAG, "instance initializer: ", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void showLoginScreen() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    private void signUp(View v) {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+
+
 }
